4aeca2d2975ad4442f953d65defe0b5b11633c1c
fedetaglia <fede.tagliabue@gmail.com>
2014-04-09 23:17:07 +1000

    fix amazon link for image

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,8 +82,10 @@ SerenaVaccari::Application.configure do
 
   # config/environments/production.rb
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
+    storage: :s3,
+    url: ':s3_domain_url',
+    path: '/:class/:attachment/:id_partition/:style/:filename',
+    s3_credentials: {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
