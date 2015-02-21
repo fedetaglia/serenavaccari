f7301a3ef55431ac55fc89e8906aa9d4c7ff565f
federico tagliabue <fede.tagliabue@gmail.com>
2015-02-21 14:49:00 +1100

    re add underscore.js

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,11 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "150x150>" }
+  has_attached_file :image, 
+                    :styles => { 
+                      :medium => "300x300>", 
+                      :thumb => "150x150>" 
+                    }, 
+                    :default_url => "/images/:style/missing.png"
+                    
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :project
