2417800d2d9b6111b6cd38f6ee9f9606a5e4d40e
federico tagliabue <fede.tagliabue@gmail.com>
2015-07-19 12:25:44 +1000

    photo position

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,13 +1,14 @@
 class Photo < ActiveRecord::Base
-  has_attached_file :image, 
-                    :styles => { 
-                      :medium => "500x500>", 
-                      :thumb => "150x150>" 
-                    }, 
+  has_attached_file :image,
+                    :styles => {
+                      :medium => "500x500>",
+                      :thumb => "150x150>"
+                    },
                     :default_url => "/images/:style/missing.png"
-                    
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   belongs_to :project
-  
+
+  default_scope { order(position: :asc) }
+
 end
